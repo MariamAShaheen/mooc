99b0222f4b81a966442dc99a1dc028fcdd8f5b43
mariamAhmedos <mariam2500ahmed@gmail.com>
2016-05-04 17:31:10 +0300

    add download link to lecture

--- app/controllers/lictures_controller.rb
+++ app/controllers/lictures_controller.rb
@@ -1,9 +1,18 @@
 class LicturesController < InheritedResources::Base
 
+ def download
+      attach = Licture.find(params[:id]).attachment.path
+     send_file attach
+
+  end
   private
 
     def licture_params
       params.require(:licture).permit(:content, :attachment, :course_id)
     end
-end
+
+    
+  
+  
+  end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :courses
  devise_for :users, controllers: { sessions: "users/sessions" , registrations: "users/registrations" }
   get 'mooc/welcome'
-
+  
+get 'lictures/:id/download', to: 'lictures#download',as:'download_licture'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
