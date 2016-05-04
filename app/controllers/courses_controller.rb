0ca06df9ccefa2de25e54527e33a34debd315dc9
mariamAhmedos <mariam2500ahmed@gmail.com>
2016-05-04 14:37:36 +0300

    authenticate the correct user courses and lecture

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,22 @@
 class CoursesController < InheritedResources::Base
+before_action :true_user, only: [:edit,:update,:destroy]
+before_action :authenticate_user!, :except => [:show]
 
   private
-
+  
     def course_params
       params.require(:course).permit(:title, :user_id)
     end
-end
+    def true_user
+      #redirect_to root_path unless course.user.id==current_user.id
+      #if current_user != course.find(params[:id]).user
+      #redirect_to root_path
+    
+      @user = User.find(params[:id])
+      redirect_to root_path 
+      end
+    
+    end
+	
+
 
