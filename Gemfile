5a0f69f32754b142537c4b4719047eba63db56b4
mariamAhmedos <mariam2500ahmed@gmail.com>
2016-05-05 19:15:54 +0300

    add   bootstrap

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 #new gems "auth" , "admin area" , "db"
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 gem 'activeadmin'
 gem 'devise'
 gem 'mysql2'
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,12 +9,13 @@ before_action :authenticate_user!, :except => [:show, :index]
       params.require(:course).permit(:title, :user_id)
     end
     def true_user
-      #redirect_to root_path unless course.user.id==current_user.id
+      @course = Course.find(params[:id])
+      redirect_to root_path unless @course.user.id==current_user.id
       #if current_user != course.find(params[:id]).user
       #redirect_to root_path
     
-      @user = User.find(params[:id])
-      redirect_to root_path 
+      #@user = User.find(params[:id])
+     # redirect_to root_path 
       end
       
    
